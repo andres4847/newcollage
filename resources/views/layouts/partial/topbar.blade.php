89a25b8c2da5c519be543b26d23494d7ea24f546
andres <silvaandres@hotmail.com>
2017-11-23 14:50:08 -0300

    top bar css

--- resources/views/layouts/partial/topbar.blade.php
+++ resources/views/layouts/partial/topbar.blade.php
@@ -1,11 +1,11 @@
-<div class="top-bar solid-bg">
-    <div class="left-widgets mini-widgets">
-        <span class="mini-contacts phone show-on-desktop near-logo-first-switch in-menu-second-switch first">011 322 44 56</span>
-        <span class="mini-contacts skype show-on-desktop in-menu-first-switch in-menu-second-switch">Seven_Marketing</span>
-        <span class="mini-contacts email show-on-desktop in-menu-first-switch in-menu-second-switch last">sales@yoursite.com</span>
-    </div>
-    <div class="right-widgets mini-widgets">
-        <div class="">
+<div class="top-bar">
+  <div class="container">
+        <div class="left-widgets">
+            <span class="glyphicon glyphicon-earphone"></span> <span class="top-contact"> 011 322 44 56</span>
+            {{--  <span class="">Seven_Marketing</span>  --}}
+            <span class="glyphicon glyphicon-envelope"></span> <span class="top-contact"> contacto@newcollage.com.ar</span>
+        </div> 
+        <div class="right-widgets">
             <a title="Facebook" href="https://www.facebook.com/WordPress.Premium.Theme/" target="_blank" class="facebook" style="visibility: visible;">
                 <span class="soc-font-icon"></span>
                 <span class="screen-reader-text">Facebook</span>
@@ -23,9 +23,9 @@
                 <span class="screen-reader-text">YouTube</span>
             </a>
             <a title="Instagram" href="https://www.instagram.com/dream_theme_photo/" target="_blank" class="instagram" style="visibility: visible;">
-              <span class="soc-font-icon"></span>
-              <span class="screen-reader-text">Instagram</span>
+            <span class="soc-font-icon"></span>
+            <span class="screen-reader-text">Instagram</span>
             </a>
-        </div>
+        </div>       
     </div>
 </div>
